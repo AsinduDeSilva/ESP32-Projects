6488256b23e0f4be345b17fc7f3d91102b37222b
Asindu De Silva <asindudesilva@gmail.com>
2025-05-06 17:05:53 +0530

    Set up ESP32 web server to control LED via browser

---
+++ Init/Init.ino
@@ -0,0 +1,100 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <Update.h>
+
+const char* ssid = "SLT Repeat";
+const char* password = "JT%479#jAB";
+
+const int ledPin = 2;
+
+WebServer server(80);
+
+
+void handleRoot() {
+  String html = "<h2>ESP32 Web Server</h2>";
+  html += "<p><a href=\"/on\">Turn LED ON</a></p>";
+  html += "<p><a href=\"/off\">Turn LED OFF</a></p>";
+  html += "<p><a href=\"/update\">OTA Update</a></p>";
+  server.send(200, "text/html", html);
+}
+
+void handleOn() {
+  digitalWrite(ledPin, HIGH);
+  server.sendHeader("Location", "/");
+  server.send(303);
+}
+
+void handleOff() {
+  digitalWrite(ledPin, LOW);
+  server.sendHeader("Location", "/");
+  server.send(303);
+}
+
+void handleUpdate() {
+  String html = R"rawliteral(
+    <h2>OTA Firmware Update</h2>
+    <form method='POST' action='/update' enctype='multipart/form-data' onsubmit='showMessage()'>
+      <input type='file' name='update'>
+      <input type='submit' value='Upload'>
+    </form>
+    <script>
+      function showMessage() {
+        alert("Update uploaded! Device will reboot shortly.");
+      }
+    </script>
+  )rawliteral";
+  server.send(200, "text/html", html);
+}
+
+void handleUpdateUpload() {
+  HTTPUpload& upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START) {
+    Serial.printf("Update: %s\n", upload.filename.c_str());
+    if (!Update.begin()) {
+      Update.printError(Serial);
+    }
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+      Update.printError(Serial);
+    }
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (Update.end(true)) {
+      Serial.println("Update Success: Rebooting...");
+      server.send(200, "text/plain", "OK");
+      delay(100); 
+      ESP.restart();
+    } else {
+      Update.printError(Serial);
+      server.send(500, "text/plain", "Update failed");
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to WiFi!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  server.on("/on", handleOn);
+  server.on("/off", handleOff);
+  server.on("/update", HTTP_GET, handleUpdate);
+  server.on("/update", HTTP_POST, []() {}, handleUpdateUpload);
+
+  server.begin();
+  Serial.println("Web server started.");
+}
+
+void loop() {
+  server.handleClient();
+}
